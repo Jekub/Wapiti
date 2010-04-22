04668c6dde1fda86550ae1db811ed2a33eb5313f
Thomas Lavergne <thomas.lavergne@reveurs.org>
2010-04-22 22:53:52 +0200

    Added l-bfgs and owl-qn optimizers

--- src/wapiti.c
+++ src/wapiti.c
@@ -172,9 +172,14 @@ static char *xstrdup(const char *str) {
  */
 typedef struct opt_s opt_t;
 struct opt_s {
+	int    maxiter;
 	int    nthread;
 	bool   sparse;
 	double rho1, rho2;
+	struct {
+		int histsz;
+		int maxls;
+	} lbfgs;
 };
 
 /******************************************************************************
@@ -2747,6 +2752,219 @@ static double grd_gradient(mdl_t *mdl, double *g, double *pg) {
 		}
 	}
 	return fx;
+}
+
+/******************************************************************************
+ *                        Quasi-Newton optimizer
+ *
+ * This section implement the quasi-Newton optimizer. We use the L-BFGS
+ * algorithm described by Liu and Nocedal in [1] and [2]. If an l1-norm must be
+ * applyed we fallback on the OWL-QN variant described in [3] by Galen and
+ * Jianfeng which allow to use L-BFGS for function not differentiable in 0.0.
+ *
+ *   [1] Updating quasi-Newton matrices with limited storage, Jorge Nocedal, in
+ *       Mathematics of Computation, vol. 35(151) 773-782, July 1980.
+ *   [2] On the limited memory BFGS method for large scale optimization, Dong C.
+ *       Liu and Jorge Nocedal, in Mathematical Programming, vol. 45(1) 503-528,
+ *       January 1989.
+ *   [3] Scalable Training of L1-Regularized Log-Linear Models, Andrew Galen and
+ *       Gao Jianfeng, in Proceedings of the 24th International Conference on
+ *       Machine Learning (ICML), Corvallis, OR, 2007.
+ *
+ ******************************************************************************/
+
+static void trn_lbfgs(mdl_t *mdl) {
+	const size_t F = mdl->nftr;
+	const int    K = mdl->opt->maxiter;
+	const int    M = mdl->opt->lbfgs.histsz;
+	const bool l1 = mdl->opt->rho1 != 0.0;
+	double *x, *xp; // Current and previous value of the variables
+	double *g, *gp; // Current and previous value of the gradient
+	double *pg;     // The pseudo-gradient (only for owl-qn)
+	double *d;      // The search direction
+	double *s[M];   // History value s_k = Δ(x,px)
+	double *y[M];   // History value y_k = Δ(g,pg)
+	double  p[M];   // ρ_k
+	// Initialization: Here, we have to allocate memory on the heap as we
+	// cannot request so much memory on the stack as this will have a too
+	// big impact on performance and will be refused by the system on non-
+	// trivial models.
+	// To make things simpler, we allocate all the memory in one call to
+	// malloc and dispatch memory in the various arrays. The main pointer
+	// will remain in the raw variable to be freed at the end.
+	double *raw = xmalloc(sizeof(double) * F * (4 + M * 2 + l1));
+	double *tmp = raw;
+	x  = mdl->theta;
+	xp = tmp; tmp += F; g = tmp; tmp += F;
+	gp = tmp; tmp += F; d = tmp; tmp += F;
+	for (int m = 0; m < M; m++) {
+		s[m] = tmp; tmp += F;
+		y[m] = tmp; tmp += F;
+	}
+	pg = l1 ? tmp : NULL;
+	// Minimization: This is the heart of the function. (a big heart...) We
+	// will perform iterations until one these conditions is reached
+	//   - the maximum iteration count is reached
+	//   - we have converged (upto numerical precision)
+	//   - the report function return false
+	//   - an error happen somewhere
+	double fx = grd_gradient(mdl, g, pg);
+	for (int k = 0; !uit_stop; k++) {
+		// 1st step: We compute the search direction. We search in the
+		// direction who minimize the second order approximation given
+		// by the Taylor series which give
+		//   d_k = - H_k^{-1} g_k
+		// But computing the inverse of the hessian is intractable so
+		// the l-bfgs only approximate it's diagonal. The exact
+		// computation is well described in [1, pp 779].
+		// The only special thing for owl-qn here is to use the pseudo
+		// gradient instead of the true one.
+		for (size_t f = 0; f < F; f++)
+			d[f] = l1 ? -pg[f] : g[f];
+		if (k != 0) {
+			const int km = k % M;
+			const int bnd = (k <= M) ? k : M;
+			double alpha[M], beta;
+			// α_i = ρ_j s_j^T q_{i+1}
+			// q_i = q_{i+1} - α_i y_i
+			for (int i = bnd; i > 0; i--) {
+				const int j = (k - i + M) % M;
+				alpha[i - 1] = p[j] * xvm_dot(s[j], d, F);
+				xvm_axpy(d, -alpha[i - 1], y[j], d, F);
+			}
+			// r_0 = H_0 q_0
+			//     Scaling is described in [2, pp 515]
+			//     for k = 0: H_0 = I
+			//     for k > 0: H_0 = I * y_k^T s_k / ||y_k||²
+			//                    = I * 1 / ρ_k ||y_k||²
+			const double y2 = xvm_dot(y[km], y[km], F);
+			const double v = 1.0 / (p[km] * y2);
+			for (size_t f = 0; f < F; f++)
+				d[f] *= v;
+			// β_j     = ρ_j y_j^T r_i
+			// r_{i+1} = r_i + s_j (α_i - β_i)
+			for (int i = 0; i < bnd; i++) {
+				const int j = (k - i - 1 + M) % M;
+				beta = p[j] * xvm_dot(y[j], d, F);
+				xvm_axpy(d, alpha[i] - beta, s[j], d, F);
+			}
+		}
+		// For owl-qn, we must remain in the same orthant than the
+		// pseudo-gradient, so we have to constrain the search
+		// direction as described in [3, pp 35(3)]
+		//   d^k = π(d^k ; v^k)
+		//       = π(d^k ; -◇f(x^k))
+		if (l1)
+			for (size_t f = 0; f < F; f++)
+				if (d[f] * pg[f] >= 0.0)
+					d[f] = 0.0;
+		// 2nd step: we perform a linesearch in the computed direction,
+		// we search a step value that satisfy the constrains using a
+		// backtracking algorithm. Much elaborated algorithm can perform
+		// better in the general case, but for CRF training, bactracking
+		// is very efficient and simple to implement.
+		// For quasi-Newton, the natural step is 1.0 so we start with
+		// this one and reduce it only if it fail with an exception for
+		// the first step where a better guess can be done.
+		// We have to keep track of the current point and gradient as we
+		// will need to compute the delta between those and the found
+		// point, and perhaps need to restore them if linesearch fail.
+		memcpy(xp, x, sizeof(double) * F);
+		memcpy(gp, g, sizeof(double) * F);
+		double gd = l1 ? 0.0 : xvm_dot(g, d, F); // gd = g_k^T d_k
+		double stp = 1.0, fi = fx;
+		if (k == 0)
+			stp = 1.0 / xvm_norm(d, F);
+		double sc = 0.5;
+		bool err = false;
+		for (int ls = 1; !uit_stop; ls++, stp *= sc) {
+			// We compute the new point using the current step and
+			// search direction
+			xvm_axpy(x, stp, d, xp, F);
+			// For owl-qn, we have to project back the point in the
+			// current orthant [3, pp 35]
+			//   x^{k+1} = π(x^k + αp^k ; ξ)
+			if (l1) {
+				for (size_t f = 0; f < F; f++) {
+					double or = xp[f];
+					if (or == 0.0)
+						or = -pg[f];
+					if (x[f] * or <= 0.0)
+						x[f] = 0.0;
+				}
+			}
+			// And we ask for the value of the objective function
+			// and its gradient and pseudo gradient.
+			fx = grd_gradient(mdl, g, pg);
+			// Now we check if the step satisfy the conditions. For
+			// l-bfgs, we check the classical decrease and curvature
+			// known as the Wolfe conditions [2, pp 506]
+			//   f(x_k + α_k d_k) ≤ f(x_k) + β' α_k g_k^T d_k
+			//   g(x_k + α_k d_k)^T d_k ≥ β g_k^T d_k
+			//
+			// And for owl-qn we check a variant of the Armijo rule
+			// described in [3, pp 36]
+			//   f(π(x^k+αp^k;ξ)) ≤ f(x^k) - γv^T[π(x^k+αp^k;ξ)-x^k]
+			if (!l1) {
+				if (fx > fi + stp * gd * 1e-4)
+					sc = 0.5;
+				else if (xvm_dot(g, d, F) < gd * 0.9)
+					sc = 2.1;
+				else
+					break;
+			} else {
+				double vp = 0.0;
+				for (size_t f = 0; f < F; f++)
+					vp += (x[f] - xp[f]) * d[f];
+				if (fx < fi + vp * 1e-4)
+					break;
+			}
+			// If we reach the maximum number of linesearsh steps
+			// without finding a good one, we just fail.
+			if (ls == mdl->opt->lbfgs.maxls) {
+				warning("maximum linesearch reached");
+				err = true;
+				break;
+			}
+		}
+		// If linesearch failed or user interupted training, we return
+		// to the last valid point and stop the training. The model is
+		// probably not fully optimized but we let the user decide what
+		// to do with it.
+		if (err || uit_stop) {
+			memcpy(x, xp, sizeof(double) * F);
+			break;
+		}
+		if (uit_progress(mdl, k + 1, fx) == false)
+			break;
+		// 3rd step: We check for convergence and if not, we update the
+		// history to prepare the next iteration. The convergence check
+		// is quite simple [2, pp 508]
+		//   ||g|| / max(1, ||x||) ≤ ε
+		// with ε small enough so we stop when numerical precision is
+		// reached. For owl-qn we just have to check against the pseudo-
+		// gradient instead of the true one.
+		const double xn = xvm_norm(x, F);
+		const double gn = xvm_norm(l1 ? pg : g, F);
+		if (gn / max(xn, 1.0) <= 1e-5)
+			break;
+		if (k + 1 == K)
+			break;
+		// So, last we update the history used for approximating the
+		// inverse of the diagonal of the hessian
+		//   s_k = x_{k+1} - x_k
+		//   y_k = g_{k+1} - g_k
+		//   ρ_k = 1 / y_k^T s_k
+		const int kn = (k + 1) % M;
+		for (size_t f = 0; f < F; f++) {
+			s[kn][f] = x[f] - xp[f];
+			y[kn][f] = g[f] - gp[f];
+		}
+		p[kn] = 1.0 / xvm_dot(y[kn], s[kn], F);
+	}
+	// Cleanup: This is very simple as we have carefully allocated memory in
+	// a sigle block, we must not forget to free it.
+	free(raw);
 }
 
 /*******************************************************************************
