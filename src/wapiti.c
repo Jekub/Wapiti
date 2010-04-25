65ae9ec8f7803f65b647cbc422189e9ee633cd25
Thomas Lavergne <thomas.lavergne@reveurs.org>
2010-04-25 21:36:26 +0200

    Added main training procedure

--- src/wapiti.c
+++ src/wapiti.c
@@ -1934,14 +1934,14 @@ static void mdl_sync(mdl_t *mdl) {
 		free(mdl->boff);  mdl->boff  = NULL;
 		free(mdl->theta); mdl->theta = NULL;
 		oldF = oldO = 0;
-		mdl->nlbl = Y;
 	}
+	mdl->nlbl = Y;
 	mdl->nobs = O;
 	// Allocate the observations datastructure. If the model is empty or
 	// discarded, a new one iscreated, else the old one is expanded.
 	char   *kind = xrealloc(mdl->kind, sizeof(char  ) * O);
-	size_t *uoff = xrealloc(mdl->uoff, sizeof(char  ) * O);
-	size_t *boff = xrealloc(mdl->boff, sizeof(char  ) * O);
+	size_t *uoff = xrealloc(mdl->uoff, sizeof(size_t) * O);
+	size_t *boff = xrealloc(mdl->boff, sizeof(size_t) * O);
 	mdl->kind = kind;
 	mdl->uoff = uoff;
 	mdl->boff = boff;
@@ -3496,6 +3496,119 @@ static void trn_sgdl1(mdl_t *mdl) {
 #undef applypenalty
 
 /*******************************************************************************
+ * Training
+ ******************************************************************************/
+static const struct {
+	char *name;
+	void (* train)(mdl_t *mdl);
+} trn_lst[] = {
+	{"l-bfgs", trn_lbfgs},
+	{"sgd-l1", trn_sgdl1},
+};
+static const int trn_cnt = sizeof(trn_lst) / sizeof(trn_lst[0]);
+
+static void dotrain(mdl_t *mdl) {
+	// Check if the user requested the trainer list. If this is not the
+	// case, search the trainer.
+	if (!strcmp(mdl->opt->algo, "list")) {
+		info("Available training algorithms:\n");
+		for (int i = 0; i < trn_cnt; i++)
+			info("\t%s\n", trn_lst[i].name);
+		exit(EXIT_SUCCESS);
+	}
+	int trn;
+	for (trn = 0; trn < trn_cnt; trn++)
+		if (!strcmp(mdl->opt->algo, trn_lst[trn].name))
+			break;
+	if (trn == trn_cnt)
+		fatal("unknown algorithm '%s'", mdl->opt->algo);
+	// Load a previous model to train again if specified by the user.
+	if (mdl->opt->pattern == NULL && mdl->opt->model == NULL)
+		fatal("you must specify either a pattern or a model");
+	if (mdl->opt->model != NULL) {
+		info("* Load previous model\n");
+		FILE *file = fopen(mdl->opt->model, "r");
+		if (file == NULL)
+			pfatal("cannot open input model file");
+		mdl_load(mdl, file);
+	}
+	// Load the pattern file. This is mandatory if no models was loaded as
+	// we need some patterns to load data files. This will unlock the
+	// database if previously locked by loading a model.
+	if (mdl->opt->pattern != NULL) {
+		info("* Load patterns\n");
+		FILE *file = fopen(mdl->opt->pattern, "r");
+		if (file == NULL)
+			pfatal("cannot open pattern file");
+		rdr_loadpat(mdl->reader, file);
+		fclose(file);
+		mdl->reader->obs->lock = false;
+	}
+	if (mdl->reader->npats == 0)
+		fatal("no patterns, cannot load input data");
+	// Load the training data. When this is done we lock the quarks as we
+	// don't want to put in the model, informations present only in the
+	// devlopment set.
+	info("* Load training data\n");
+	FILE *file = stdin;
+	if (mdl->opt->input != NULL) {
+		file = fopen(mdl->opt->input, "r");
+		if (file == NULL)
+			pfatal("cannot open input data file");
+	}
+	mdl->train = rdr_readdat(mdl->reader, file, true);
+	if (mdl->opt->input != NULL)
+		fclose(file);
+	mdl->reader->lbl->lock = true;
+	mdl->reader->obs->lock = true;
+	if (mdl->train->nseq == 0)
+		fatal("no train data loaded");
+	// If present, load the development set in the model. If not specified,
+	// the training dataset will be used instead.
+	if (mdl->opt->devel != NULL) {
+		info("* Load development data\n");
+		FILE *file = fopen(mdl->opt->devel, "r");
+		if (file == NULL)
+			pfatal("cannot open development file");
+		mdl->devel = rdr_readdat(mdl->reader, file, true);
+		fclose(file);
+	}
+	// Initialize the model. If a previous model was loaded, this will be
+	// just a resync, else the model structure will be created.
+	if (mdl->theta == NULL)
+		info("* Initialize the model\n");
+	else
+		info("* Resync the model\n");
+	mdl_sync(mdl);
+	// Display some statistics as we all love this.
+	info("* Summary\n");
+	info("    nb labels:   %zu\n", mdl->nlbl);
+	info("    nb blocks:   %zu\n", mdl->nobs);
+	info("    nb features: %zu\n", mdl->nftr);
+	// And train the model...
+	info("* Train the model with %s\n", mdl->opt->algo);
+	uit_setup(mdl);
+	trn_lst[trn].train(mdl);
+	uit_cleanup(mdl);
+	// If requested compact the model.
+	if (mdl->opt->compact) {
+		warning("compacting not implemented");
+	}
+	// And save the trained model
+	info("* Save the model\n");
+	file = stdout;
+	if (mdl->opt->output != NULL) {
+		file = fopen(mdl->opt->output, "w");
+		if (file == NULL)
+			pfatal("cannot open output model");
+	}
+	mdl_save(mdl, file);
+	if (mdl->opt->output != NULL)
+		fclose(file);
+	info("* Done\n");
+}
+
+/*******************************************************************************
  * Entry point
  ******************************************************************************/
 int main(int argc, char *argv[argc]) {
@@ -3508,7 +3621,7 @@ int main(int argc, char *argv[argc]) {
 	// And switch to requested mode
 	switch (opt.mode) {
 		case 0:
-			fatal("training not implemented");
+			dotrain(mdl);
 			break;
 		case 1:
 			fatal("labeling not implemented");
