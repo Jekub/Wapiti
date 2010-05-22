d619a46e82fd3fb261476449c6eba8ad4bd835a8
Thomas Lavergne <thomas.lavergne@reveurs.org>
2010-05-22 10:52:00 +0200

    Move model to model.{h,c}

---
+++ src/model.c
@@ -0,0 +1,282 @@
+/*
+ *      Wapiti - A linear-chain CRF tool
+ *
+ * Copyright (c) 2009-2010  CNRS
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the CNRS, nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "model.h"
+#include "options.h"
+#include "quark.h"
+#include "reader.h"
+#include "tools.h"
+
+/*******************************************************************************
+ * Linear chain CRF model
+ *
+ *   There is three concept that must be well understand here, the labels,
+ *   observations, and features. The labels are the values predicted by the
+ *   model at each point of the sequence and denoted by Y. The observations are
+ *   the values, at each point of the sequence, given to the model in order to
+ *   predict the label and denoted by O. A feature is a test on both labels and
+ *   observations, denoted by F. In linear chain CRF there is two kinds of
+ *   features :
+ *     - unigram feature who represent a test on the observations at the current
+ *       point and the label at current point.
+ *     - bigram feature who represent a test on the observation at the current
+ *       point and two labels : the current one and the previous one.
+ *   So for each observation, there Y possible unigram features and Y*Y possible
+ *   bigram features. The kind of features used by the model for a given
+ *   observation depend on the pattern who generated it.
+ ******************************************************************************/
+
+/* mdl_new:
+ *   Allocate a new empty model object linked with the given reader. The model
+ *   have to be synchronized before starting training or labelling. If you not
+ *   provide a reader (as it will loaded from file for example) you must be sure
+ *   to set one in the model before any attempts to synchronize it.
+ */
+mdl_t *mdl_new(rdr_t *rdr) {
+	mdl_t *mdl = xmalloc(sizeof(mdl_t));
+	mdl->nlbl   = mdl->nobs  = mdl->nftr = 0;
+	mdl->kind   = NULL;
+	mdl->uoff   = mdl->boff  = NULL;
+	mdl->theta  = NULL;
+	mdl->train  = mdl->devel = NULL;
+	mdl->reader = rdr;
+	mdl->werr   = NULL;
+	mdl->total  = 0.0;
+	return mdl;
+}
+
+/* mdl_free:
+ *   Free all memory used by a model object inculding the reader and datasets
+ *   loaded in the model.
+ */
+void mdl_free(mdl_t *mdl) {
+	free(mdl->kind);
+	free(mdl->uoff);
+	free(mdl->boff);
+	free(mdl->theta);
+	if (mdl->train != NULL)
+		rdr_freedat(mdl->train);
+	if (mdl->devel != NULL)
+		rdr_freedat(mdl->devel);
+	if (mdl->reader != NULL)
+		rdr_free(mdl->reader);
+	if (mdl->werr != NULL)
+		free(mdl->werr);
+}
+
+/* mdl_sync:
+ *   Synchronize the model with its reader. As the model is just a placeholder
+ *   for features weights and interned sequences, it know very few about the
+ *   labels and observations, all the informations are kept in the reader. A
+ *   sync will get the labels and observations count as well as the observation
+ *   kind from the reader and build internal structures representing the model.
+ *
+ *   If the model was already synchronized before, there is an existing model
+ *   incompatible with the new one to be created. In this case there is two
+ *   possibility :
+ *     - If only new observations was added, the weights of the old ones remain
+ *       valid and are kept as they form a probably good starting point for
+ *       training the new model, the new observation get a 0 weight ;
+ *     - If new labels was added, the old model are trully meaningless so we
+ *       have to fully discard them and build a new empty model.
+ *   In any case, you must never change existing labels or observations, if this
+ *   happen, you need to create a new model and destroy this one.
+ *
+ *   After synchronization, the labels and observations databases are locked to
+ *   prevent new one to be created. You must unlock them explicitly if needed.
+ *   This reduce the risk of mistakes.
+ */
+void mdl_sync(mdl_t *mdl) {
+	const size_t Y = qrk_count(mdl->reader->lbl);
+	const size_t O = qrk_count(mdl->reader->obs);
+	// If model is already synchronized, do nothing and just return
+	if (mdl->nlbl == Y && mdl->nobs == O)
+		return;
+	if (Y == 0 || O == 0)
+		fatal("cannot synchronize an empty model");
+	// If new labels was added, we have to discard all the model. In this
+	// case we also display a warning as this is probably not expected by
+	// the user. If only new observations was added, we will try to expand
+	// the model.
+	size_t oldF = mdl->nftr;
+	size_t oldO = mdl->nobs;
+	if (mdl->nlbl != Y && mdl->nlbl != 0) {
+		warning("labels count changed, discarding the model");
+		free(mdl->kind);  mdl->kind  = NULL;
+		free(mdl->uoff);  mdl->uoff  = NULL;
+		free(mdl->boff);  mdl->boff  = NULL;
+		free(mdl->theta); mdl->theta = NULL;
+		oldF = oldO = 0;
+	}
+	mdl->nlbl = Y;
+	mdl->nobs = O;
+	// Allocate the observations datastructure. If the model is empty or
+	// discarded, a new one iscreated, else the old one is expanded.
+	char   *kind = xrealloc(mdl->kind, sizeof(char  ) * O);
+	size_t *uoff = xrealloc(mdl->uoff, sizeof(size_t) * O);
+	size_t *boff = xrealloc(mdl->boff, sizeof(size_t) * O);
+	mdl->kind = kind;
+	mdl->uoff = uoff;
+	mdl->boff = boff;
+	// Now, we can setup the features. For each new observations we fill the
+	// kind and offsets arrays and count total number of features as well.
+	size_t F = oldF;
+	for (size_t o = oldO; o < O; o++) {
+		const char *obs = qrk_id2str(mdl->reader->obs, o);
+		switch (obs[0]) {
+			case 'u': kind[o] = 1; break;
+			case 'b': kind[o] = 2; break;
+			case '*': kind[o] = 3; break;
+		}
+		if (kind[o] & 1)
+			uoff[o] = F, F += Y;
+		if (kind[o] & 2)
+			boff[o] = F, F += Y * Y;
+	}
+	mdl->nftr = F;
+	// We can finally grow the features weights vector itself. We set all
+	// the new features to 0.0 but don't touch the old ones.
+	mdl->theta = xrealloc(mdl->theta, sizeof(double) * F);
+	for (size_t f = oldF; f < F; f++)
+		mdl->theta[f] = 0.0;
+	// And lock the databases
+	qrk_lock(mdl->reader->lbl, true);
+	qrk_lock(mdl->reader->obs, true);
+}
+
+/* mdl_compact:
+ *   Comapct the given model by removing from it all observation who lead to
+ *   zero actives features. On model trained with l1 regularization this can
+ *   lead to a drastic model size reduction and so to faster loading, training
+ *   and labeling.
+ */
+void mdl_compact(mdl_t *mdl) {
+	const size_t Y = mdl->nlbl;
+	// We first build the new observation list with only observations which
+	// lead to at least one active feature. At the same time we build the
+	// translation table which map the new observations index to the old
+	// ones.
+	info("    - Scan the model\n");
+	qrk_t *old_obs = mdl->reader->obs;
+	qrk_t *new_obs = qrk_new();
+	size_t *trans = xmalloc(sizeof(size_t) * mdl->nobs);
+	for (size_t oldo = 0; oldo < mdl->nobs; oldo++) {
+		bool active = false;
+		if (mdl->kind[oldo] & 1)
+			for (size_t y = 0; y < Y; y++)
+				if (mdl->theta[mdl->uoff[oldo] + y] != 0.0)
+					active = true;
+		if (mdl->kind[oldo] & 2)
+			for (size_t d = 0; d < Y * Y; d++)
+				if (mdl->theta[mdl->boff[oldo] + d] != 0.0)
+					active = true;
+		if (!active)
+			continue;
+		const char   *str  = qrk_id2str(old_obs, oldo);
+		const size_t  newo = qrk_str2id(new_obs, str);
+		trans[newo] = oldo;
+	}
+	mdl->reader->obs = new_obs;
+	// Now we save the old model features informations and build a new one
+	// corresponding to the compacted model.
+	size_t *old_uoff  = mdl->uoff;  mdl->uoff  = NULL;
+	size_t *old_boff  = mdl->boff;  mdl->boff  = NULL;
+	double *old_theta = mdl->theta; mdl->theta = NULL;
+	free(mdl->kind);
+	mdl->kind = NULL;
+	mdl->nlbl = mdl->nobs = mdl->nftr = 0;
+	mdl_sync(mdl);
+	// The model is now ready, so we copy in it the features weights from
+	// the old model for observations we have kept.
+	info("    - Compact it\n");
+	for (size_t newo = 0; newo < mdl->nobs; newo++) {
+		const size_t oldo = trans[newo];
+		if (mdl->kind[newo] & 1) {
+			double *src = old_theta  + old_uoff[oldo];
+			double *dst = mdl->theta + mdl->uoff[newo];
+			for (size_t y = 0; y < Y; y++)
+				dst[y] = src[y];
+		}
+		if (mdl->kind[newo] & 2) {
+			double *src = old_theta  + old_boff[oldo];
+			double *dst = mdl->theta + mdl->boff[newo];
+			for (size_t d = 0; d < Y * Y; d++)
+				dst[d] = src[d];
+		}
+	}
+	// And cleanup
+	free(trans);
+	qrk_free(old_obs);
+	free(old_uoff);
+	free(old_boff);
+	free(old_theta);
+}
+
+/* mdl_save:
+ *   Save a model to be restored later in a platform independant way.
+ */
+void mdl_save(mdl_t *mdl, FILE *file) {
+	size_t nact = 0;
+	for (size_t f = 0; f < mdl->nftr; f++)
+		if (mdl->theta[f] != 0.0)
+			nact++;
+	fprintf(file, "#mdl#%zu\n", nact);
+	rdr_save(mdl->reader, file);
+	for (size_t f = 0; f < mdl->nftr; f++)
+		if (mdl->theta[f] != 0.0)
+			fprintf(file, "%zu=%la\n", f, mdl->theta[f]);
+}
+
+/* mdl_load:
+ *   Read back a previously saved model to continue training or start labeling.
+ *   The returned model is synced and the quarks are locked. You must give to
+ *   this function an empty model fresh from mdl_new.
+ */
+void mdl_load(mdl_t *mdl, FILE *file) {
+	const char *err = "invalid model format";
+	size_t nact = 0;
+	if (fscanf(file, "#mdl#%zu\n", &nact) != 1)
+		fatal(err);
+	rdr_load(mdl->reader, file);
+	mdl_sync(mdl);
+	for (size_t i = 0; i < nact; i++) {
+		size_t f;
+		double v;
+		if (fscanf(file, "%zu=%la\n", &f, &v) != 2)
+			fatal(err);
+		mdl->theta[f] = v;
+	}
+}
+
--- src/model.h
+++ src/model.h
@@ -31,4 +31,72 @@
 #ifndef model_h
 #define model_h
 
+#include <stddef.h>
+#include <sys/times.h>
+
+#include "options.h"
+#include "sequence.h"
+#include "reader.h"
+
+typedef struct tms tms_t;
+
+/* mdl_t:
+ *   Represent a linear-chain CRF model. The model contain both unigram and
+ *   bigram features. It is caracterized by <nlbl> the number of labels, <nobs>
+ *   the number of observations, and <nftr> the number of features.
+ *
+ *   Each observations have a corresponding entry in <kind> whose first bit is
+ *   set if the observation is unigram and second one if it is bigram. Note that
+ *   an observation can be both. An unigram observation produce Y features and a
+ *   bigram one produce Y * Y features.
+ *   The <theta> array keep all features weights. The <*off> array give for each
+ *   observations the offset in the <theta> array where the features of the
+ *   observation are stored.
+ *
+ *   The <*off> and <theta> array are initialized only when the model is
+ *   synchronized. As you can add new labels and observations after a sync, we
+ *   keep track of the old counts in <olbl> and <oblk> to detect inconsistency
+ *   and resynchronize the model if needed. In this case, if the number of
+ *   labels have not changed, the previously trained weights are kept, else they
+ *   are now meaningless so discarded.
+ */
+typedef struct mdl_s mdl_t;
+struct mdl_s {
+	opt_t   *opt;     //       options for training
+
+	// Size of various model parameters
+	size_t   nlbl;    //   Y   number of labels
+	size_t   nobs;    //   O   number of observations
+	size_t   nftr;    //   F   number of features
+
+	// Informations about observations
+	char    *kind;    //  [O]  observations type
+	size_t  *uoff;    //  [O]  unigram weights offset
+	size_t  *boff;    //  [O]  bigram weights offset
+
+	// The model itself
+	double  *theta;   //  [F]  features weights
+
+	// Datasets
+	dat_t   *train;   //       training dataset
+	dat_t   *devel;   //       development dataset
+	rdr_t   *reader;
+
+	// Stoping criterion
+	double  *werr;    //       Window of error rate of last iters
+	int      wcnt;    //       Number of iters in the window
+	int      wpos;    //       Position for the next iter
+
+	// Timing
+	tms_t    timer;   //       start time of last iter
+	double   total;   //       total training time
+};
+
+mdl_t *mdl_new(rdr_t *rdr);
+void mdl_free(mdl_t *mdl);
+void mdl_sync(mdl_t *mdl);
+void mdl_compact(mdl_t *mdl);
+void mdl_save(mdl_t *mdl, FILE *file);
+void mdl_load(mdl_t *mdl, FILE *file);
+
 #endif
