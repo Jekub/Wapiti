569fbe5040583086f8d26667f6b793dc641536b0
Thomas Lavergne <thomas.lavergne@reveurs.org>
2013-12-18 12:42:00 +0100

    Release v1.5.0
    Signed-off-by: Thomas Lavergne <thomas.lavergne@reveurs.org>

--- src/wapiti.h
+++ src/wapiti.h
@@ -27,7 +27,7 @@
 #ifndef wapiti_h
 #define wapiti_h
 
-#define VERSION "1.4.0"
+#define VERSION "1.5.0"
 
 /* XVM_ANSI:
  *   By uncomenting the following define, you can force wapiti to not use SSE2
