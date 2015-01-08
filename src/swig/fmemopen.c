e1d10daa5862886cd0180bd0bed39ca710b4e7bc
Christopher Boumenot <boumenot@gmail.com>
2015-01-07 16:15:52 -0800

    Add support for Windows.
    There is no equivalent of fmemopen or funopen on Windows.  The next best
    thing is to use temporary files on disk.  To ensure these files are
    deleted upon close they are opened with CreateFile and then translated to
    FILE type.

--- src/swig/fmemopen.c
+++ src/swig/fmemopen.c
@@ -8,7 +8,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef linux
+#if defined(WIN32) || defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+
+int init_file(const char *filename, void* buf, size_t size)
+{
+    FILE *fout = fopen(filename, "wb");
+    if (fout == NULL)
+        return 1;
+
+    fwrite(buf, size, 1, fout);
+    fclose(fout);
+
+    return 0;
+}
+
+FILE* open_file_temp(const char *filename, const char *mode)
+{
+    HANDLE hFile;
+    int fd;
+
+    hFile = CreateFile(
+        filename,
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE | FILE_FLAG_SEQUENTIAL_SCAN,
+        NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+        return NULL;
+
+    fd = _open_osfhandle((intptr_t)hFile, _O_TEXT);
+    return _fdopen(fd, mode);
+    
+}
+
+FILE *fmemopen(void *buf, size_t size, const char *mode)
+{
+    char temppath[MAX_PATH - 14];
+    if (0 == GetTempPath(sizeof(temppath), temppath))
+        return NULL;
+
+    char filename[MAX_PATH];
+    if (0 == GetTempFileName(temppath, "WAP", 0, filename))
+        return NULL;
+
+    if (0 == init_file(filename, buf, size))
+        return NULL;
+    
+    return open_file_temp(filename, mode);}
+
+
+#elif !defined(linux)
+
 struct fmem {
     size_t pos;
     size_t size;
@@ -75,4 +132,5 @@ FILE *fmemopen(void *buf, size_t size, const char *mode)
     mem->size = size, mem->buffer = buf;
     return funopen(mem, readfn, writefn, seekfn, closefn);
 }
+
 #endif
