eb1797d9b9f044cc83a448fa981b185f9e71d92e
Christopher Boumenot <boumenot@gmail.com>
2017-10-08 18:35:45 -0700

    update AppVeyor (and local) build

---
+++ clean.cmd
@@ -0,0 +1,2 @@
+rem msbuild wapiti.sln /t:Build /p:Configuration=Release
+msbuild wapiti.sln /t:Clean
--- init.cmd
+++ init.cmd
@@ -1,12 +1,11 @@
 rem Works on my machine...
-
 rm -rf CMakeCache.txt CMakeFiles
 
 rem call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
+rem set PATH=%PATH%;c:\Program Files\LLVM\bin
+rem set PATH=%PATH%;c:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64
 
 "c:\Program Files\CMake\bin\cmake.exe"^
- -DCMAKE_C_COMPILER="c:/Program Files/LLVM/bin/clang-cl.exe"^
- -DCMAKE_CXX_COMPILER="c:/Program Files/LLVM/bin/clang-cl.exe"^
- -DCMAKE_BUILD_TYPE=Release^
- -G Ninja^
- .
+  -G "Visual Studio 15 2017 Win64"^
+  -T LLVM-vs2014^
+  .
